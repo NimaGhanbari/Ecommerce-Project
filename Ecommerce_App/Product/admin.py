3c52c64eb7eb73abd13f2bc6f977b760341fbad3
nima ghanbari <nimagithub1401@gmail.com>
2023-09-03 14:54:49 +0330

    add foreignkey product and category

--- Ecommerce_App/Product/admin.py
+++ Ecommerce_App/Product/admin.py
@@ -4,4 +4,6 @@ from .models import Products
 @admin.register(Products)
 class Products_admin(admin.ModelAdmin):
     list_display = ['title','price','slug','uniqe_code','is_enable','created_at']
+    filter_horizontal = ['categories']
     search_fields = ['title']
+    date_hierarchy = 'created_at'
