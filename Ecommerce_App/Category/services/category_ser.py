956523f2a9e2920559ef82b999ad17d767ca37c8
nima ghanbari <nimagithub1401@gmail.com>
2023-09-04 14:50:14 +0330

    add selctionsort

--- Ecommerce_App/Category/services/category_ser.py
+++ Ecommerce_App/Category/services/category_ser.py
@@ -2,6 +2,10 @@ from Ecommerce_App.Category.models import Category
 
 
 def is_subcategory(categor):
+    """
+    This function returns the categories,
+    if we give None input to it, the parent categories will be returned
+    """
     return Category.objects.filter(parent=categor,is_active=True)
 
 
--- Ecommerce_App/Product/services/product_ser.py
+++ Ecommerce_App/Product/services/product_ser.py
@@ -1,6 +1,29 @@
 
-    
-    
+def bubblesort(elements):
+    for n in range(len(elements)-1, 0, -1):
+        for i in range(n):
+            if elements[i].created_at > elements[i + 1].created_at:
+                elements[i], elements[i + 1] = elements[i + 1], elements[i]
+                
+    return elements
+
+
+def selectionSort(elements,id):
+    size =len(elements)
+    for ind in range(size):
+        min_index = ind
+        for j in range(ind + 1, size):
+            # select the minimum element in every iteration
+            if id == 2:
+                if elements[j].price > elements[min_index].price:
+                    min_index = j
+            else:
+                if elements[j].price < elements[min_index].price:
+                    min_index = j
+        # swapping the elements to sort the array
+        (elements[ind], elements[min_index]) = (elements[min_index], elements[ind])
+    return elements
+
 def Sort_By(elements,by=4):
     #0:محبوبترین
     #1:پرفروشترین
@@ -12,19 +35,11 @@ def Sort_By(elements,by=4):
     elif by == 1:
         pass
     elif by ==2:
-        pass
+        selectionSort(elements=elements,id=by)
     elif by ==3:
-        pass
+        selectionSort(elements=elements,id=by)
     elif by == 4:
-        def bubblesort(elements):
-            # Looping from size of array from last index[-1] to index [0]
-            for n in range(len(elements)-1, 0, -1):
-                for i in range(n):
-                    if elements[i].created_at > elements[i + 1].created_at:
-                        swapped = True
-                        # swapping data if the element is less than next element in the array
-                        elements[i], elements[i + 1] = elements[i + 1], elements[i]
-            
-            for x in elements:
-                print(x.created_at)
-            return elements
+        elements = bubblesort(elements=elements)
+        for x in elements:
+            print(x.created_at)
+        return elements
