f670adff1a7cd4fc617f0e95d789ea05c4bfef98
nima ghanbari <nimagithub1401@gmail.com>
2023-09-04 16:27:57 +0330

    Comment was added on each product page(StackedInline)

--- Ecommerce_App/Comment/admin.py
+++ Ecommerce_App/Comment/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import Comment
+from nested_admin import NestedStackedInline
 
-@admin.register(Comment)
-class Comments_admin(admin.ModelAdmin):
+
+class Comments_admin(NestedStackedInline):
+    model = Comment
     list_display = ['author','text','product','is_active','created_at','updated_at']
     date_hierarchy = 'created_at'
+    extra = 0
 
 
--- Ecommerce_App/Product/admin.py
+++ Ecommerce_App/Product/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import Products
+from Ecommerce_App.Comment.admin import Comments_admin
+from nested_admin import NestedModelAdmin
+from Ecommerce_App.Category.models import Category
 
 @admin.register(Products)
-class Products_admin(admin.ModelAdmin):
-    list_display = ['title','price','slug','uniqe_code','is_enable','created_at']
+class Products_admin(NestedModelAdmin):
+    list_display = ['title','price','slug','uniqe_code','is_enable','categor','created_at']
+    def categor(self, obj):
+        return ([Category.title for category in obj.categories.all()])
     filter_horizontal = ['categories']
     search_fields = ['title']
     date_hierarchy = 'created_at'
+    inlines = [Comments_admin]
+    
