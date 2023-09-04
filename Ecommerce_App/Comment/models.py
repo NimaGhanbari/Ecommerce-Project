bb3c75bdebe27183dd1495b4d604d6e9c0921ad0
nima ghanbari <nimagithub1401@gmail.com>
2023-09-04 15:46:20 +0330

    add comment model

--- Ecommerce_App/Comment/models.py
+++ Ecommerce_App/Comment/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from django.utils.translation import ugettext_lazy as _
+from Ecommerce_App.Product.models import Products
+from Ecommerce_App.Commons.models import BaseModel
+class Comment(BaseModel):
+    author = models.ForeignKey(User,related_name="comments",on_delete=models.CASCADE)
+    text = models.TextField(verbose_name=_("text"))
+    product = models.ForeignKey(Products,related_name="comments",on_delete=models.CASCADE)
+    is_active = models.BooleanField(verbose_name=_("is active"),default=True)
+    
+    class Meta:
+        db_table = "Comments"
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comments")
 
-
+    def __str__(self):
+        return self.text
+        
+    
