c1203bf917f0670a424a9061c7f256d5d6cd24a2
nima ghanbari <nimagithub1401@gmail.com>
2023-09-04 12:33:09 +0330

    Revert "add parent category restorer"
    This reverts commit c0012a22d0d2283e188423f0a64bd6a2bb759d0d.

--- Ecommerce_App/Category/services/category_ser.py
+++ Ecommerce_App/Category/services/category_ser.py
@@ -1,10 +1,7 @@
 from Ecommerce_App.Category.models import Category
 
 
-def Subcategory(categor):
+def is_subcategory(categor):
     return Category.objects.filter(parent=categor,is_active=True)
-
-def ParentCategory():
-    return Category.objects.filter(parent=None,is_active=True)
 
 
