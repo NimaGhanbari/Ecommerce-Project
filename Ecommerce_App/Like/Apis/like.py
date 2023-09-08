b4bc05ccf5a4d7a2bb9f5b9db4fbb9d3b5d44f87
nima ghanbari <nimagithub1401@gmail.com>
2023-09-08 11:22:27 +0330

    create reaction api (delete,create,edit)

---
+++ Ecommerce_App/Like/Apis/like.py
@@ -0,0 +1,35 @@
+from Ecommerce_App.Like.models import Like
+from Ecommerce_App.Product.models import Products
+from Ecommerce_App.Like.services.like import create_like
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import serializers
+
+
+
+class ReactionApi(APIView):
+    #کاربر باید احراز هویت کند
+    class InPutSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Like
+            fields = ("value",)
+    
+    
+    
+    def put(self,request,Pslug):
+        serializer = self.InPutSerializer(data=request.data) 
+        serializer.is_valid(raise_exception=True)
+        product = get_object_or_404(Products,slug=Pslug)
+        try:
+            result = create_like(user=request.user,product=product,value=serializer.data["value"])
+        except Exception as ex:
+            return Response(
+                {"detail": "Database Error - " + str(ex)},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return Response(status=status.HTTP_201_CREATED)
+        
+        
+        
--- Ecommerce_App/Product/urls.py
+++ Ecommerce_App/Product/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from Ecommerce_App.Product.Apis.product import PostApi,PostDetailApi
+from Ecommerce_App.Like.Apis.like import ReactionApi
 
 urlpatterns = [
     path("list/<slug:Cslug>/",PostApi.as_view(),name="product-list"),
     path("<slug:Pslug>/",PostDetailApi.as_view(),name="product-detail"),
+    path("<slug:Pslug>/reaction/",ReactionApi.as_view(),name="product-reaction"),
     
         
 ]
