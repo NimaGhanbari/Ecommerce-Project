04aef9339b371c2958c3636ca143817ddfa6ab6a
nima ghanbari <nimagithub1401@gmail.com>
2023-09-09 13:03:28 +0330

    add admin post file

--- Ecommerce_App/PostFiles/admin.py
+++ Ecommerce_App/PostFiles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from nested_admin import NestedStackedInline
+from Ecommerce_App.PostFiles.models import Post_File
 
-# Register your models here.
+class Files_Admin(NestedStackedInline):
+    model = Post_File
+    #user add in fields
+    fields = ['id','title','file_type','fil','is_active']
+    extra = 0
--- Ecommerce_App/Product/admin.py
+++ Ecommerce_App/Product/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Products
 from Ecommerce_App.Comment.admin import Comments_admin
 from Ecommerce_App.Like.admin import Like_admin
+from Ecommerce_App.PostFiles.admin import Files_Admin
 from nested_admin import NestedModelAdmin
 from Ecommerce_App.Category.models import Category
 
@@ -13,5 +14,5 @@ class Products_admin(NestedModelAdmin):
     filter_horizontal = ['categories']
     search_fields = ['title']
     date_hierarchy = 'created_at'
-    inlines = [Comments_admin,Like_admin]
+    inlines = [Comments_admin,Like_admin,Files_Admin]
     
