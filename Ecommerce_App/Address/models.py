f2e85e0a83e18b1ebc52258397bd7c9db93f8626
nima ghanbari <nimagithub1401@gmail.com>
2023-09-18 12:55:38 +0330

    add model address

--- Ecommerce_App/Address/models.py
+++ Ecommerce_App/Address/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from Ecommerce_App.Commons.models import BaseModel
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Address(BaseModel):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    zipcode = models.IntegerField(verbose_name=_("zipcode"))
+    
+    class Meta:
+        db_table = "Address"
+        verbose_name = _("Address")
+        verbose_name_plural = _("Address")
