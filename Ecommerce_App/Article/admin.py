22e36ae19fbaf39d49fcdc593d7948175f17d0b2
nima ghanbari <nimagithub1401@gmail.com>
2023-11-10 17:01:42 +0330

    Article -> add admin

--- Ecommerce_App/Article/admin.py
+++ Ecommerce_App/Article/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from Ecommerce_App.Article.models import Article
+from Ecommerce_App.PostFiles.admin import Files_Article_Admin
 
-# Register your models here.
+# Third Party
+from nested_admin import NestedModelAdmin
+
+
+@admin.register(Article)
+class Article_Admin(NestedModelAdmin):
+    list_display = ['title', 'slug', 'is_active', 'created_at']
+    # filter_horizontal = ['categories']
+    search_fields = ['title']
+    date_hierarchy = 'created_at'
+    inlines = [Files_Article_Admin,]
