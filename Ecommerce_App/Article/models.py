25bf2dfef061a51dfd3f31536ef5821577d8271a
nima ghanbari <nimagithub1401@gmail.com>
2023-11-10 16:38:10 +0330

    article -> add models

--- Ecommerce_App/Article/models.py
+++ Ecommerce_App/Article/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from Ecommerce_App.Commons.models import BaseModel
 
-# Create your models here.
+
+
+
+class Article(BaseModel):
+    
+    title = models.CharField(max_length=128,verbose_name="عنوان")
+    text = models.TextField(verbose_name="بدنه مقاله",blank=True,null=True)
+    slug = models.SlugField(primary_key=True, max_length=100)
+    is_active = models.BooleanField(default=True)
+    uniqe_code = models.PositiveIntegerField(verbose_name="uniqe_code", unique=True)
+    class Meta:
+        db_table = "Articles"
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+
+    def __str__(self):
+        return self.title
+
