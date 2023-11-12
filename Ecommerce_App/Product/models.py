4beb100c869b3dec6083df6f10e5f2bcf322a674
nima ghanbari <nimagithub1401@gmail.com>
2023-11-12 20:10:35 +0330

    Using signals to automatically generate slugs for products and edit URLs for Persian slugs

--- Ecommerce_App/Product/models.py
+++ Ecommerce_App/Product/models.py
@@ -6,12 +6,16 @@ from django.utils.translation import ugettext_lazy as _
 from Ecommerce_App.Commons.models import BaseModel
 from Ecommerce_App.Category.models import Category
 
+from django.utils.text import slugify
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 class Products(BaseModel):
     title = models.CharField(_("title"), max_length=70)
     description = models.TextField(_("description"), blank=True)
     price = models.BigIntegerField(_("Price"))
-    slug = models.SlugField(primary_key=True, max_length=100)
+    slug = models.SlugField(primary_key=True, max_length=100, blank=True,allow_unicode=True)
     categories = models.ManyToManyField(Category, verbose_name=_("categories"))
     is_enable = models.BooleanField(_("is enable"), default=True)
     uniqe_code = models.PositiveIntegerField(
@@ -32,3 +36,22 @@ class Products(BaseModel):
         dict_count["dislike"] = self.likes.filter(value="2").count()
         dict_count["popular"] = self.likes.filter(value="3").count()
         return dict_count
+
+
+def generate_unique_slug(instance, new_slug=None):
+    slug = None
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.title, allow_unicode=True)
+    qs = Products.objects.filter(slug=slug)
+    if qs.exists():
+        new_slug = f"{slug}-{qs.count()}"
+        return generate_unique_slug(instance, new_slug=new_slug)
+    return slug
+
+
+@receiver(pre_save, sender=Products)
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = generate_unique_slug(instance)
